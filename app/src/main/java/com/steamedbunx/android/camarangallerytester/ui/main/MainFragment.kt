e27a1e65ac2e04561977773257e75ba0a717f0a1
Yi Hao <yi.hao01@outlook.com>
2019-10-08 11:49:52 -0500

    Add permission check block

--- app/src/main/java/com/steamedbunx/android/camarangallerytester/ui/main/MainFragment.kt
+++ app/src/main/java/com/steamedbunx/android/camarangallerytester/ui/main/MainFragment.kt
@@ -1,12 +1,14 @@
 package com.steamedbunx.android.camarangallerytester.ui.main
 
 import android.Manifest
+import android.content.pm.PackageManager
 import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.listener.single.DialogOnDeniedPermissionListener
@@ -16,8 +18,7 @@ import com.steamedbunx.android.camarangallerytester.databinding.MainFragmentBind
 
 class MainFragment : Fragment() {
 
-    lateinit var dialogPermissionListener:PermissionListener
-    lateinit var binding:MainFragmentBinding
+    lateinit var binding: MainFragmentBinding
 
     companion object {
         fun newInstance() = MainFragment()
@@ -36,20 +37,63 @@ class MainFragment : Fragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
         viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        dialogPermissionListener = DialogOnDeniedPermissionListener.Builder
+        binding.buttonCamera.setOnClickListener { onCameraClicked() }
+        binding.buttonGallery.setOnClickListener { onGalleryClicked() }
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        askStoragePermission()
+    }
+
+    fun onCameraClicked() {
+        askCameraPermission()
+        checkCameraPermission()
+    }
+
+    fun onGalleryClicked() {
+        askStoragePermission()
+        checkStoragePermission()
+    }
+
+    fun askCameraPermission() {
+        val dialogPermissionListener = DialogOnDeniedPermissionListener.Builder
             .withContext(requireContext())
             .withTitle("Camera permission")
             .withMessage("Camera permission is needed to take pictures of the item")
             .withButtonText(android.R.string.ok)
             .build()
-        binding.buttonCamara.setOnClickListener { askPermission() }
-    }
-
-    fun askPermission(){
         Dexter.withActivity(requireActivity())
             .withPermission(Manifest.permission.CAMERA)
             .withListener(dialogPermissionListener)
             .check()
+    }
+
+    fun askStoragePermission() {
+        val dialogPermissionListener = DialogOnDeniedPermissionListener.Builder
+            .withContext(requireContext())
+            .withTitle("Read Storage permission")
+            .withMessage("Read Storage permission is needed to load image from gallery")
+            .withButtonText(android.R.string.ok)
+            .build()
+        Dexter.withActivity(requireActivity())
+            .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+            .withListener(dialogPermissionListener)
+            .check()
+    }
+
+    fun checkStoragePermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.READ_EXTERNAL_STORAGE
+        ) == PackageManager.PERMISSION_GRANTED
+    }
+
+    fun checkCameraPermission(): Boolean {
+        return ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.CAMERA
+        ) == PackageManager.PERMISSION_GRANTED
     }
 
 }
